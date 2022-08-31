7c47402f2a72dd7bf90a67cd16beba9f9b032313
Arteandr <artedamatik@gmail.com>
2022-08-31 15:53:48 +0300

    Add "Add todo" method

--- src/simple_http/handler.clj
+++ src/simple_http/handler.clj
@@ -1,10 +1,12 @@
 (ns simple-http.handler
   (:require [compojure.core :refer :all]
-            [simple-http.todos :refer [getAllTodos]]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [simple-http.todos :refer [getAllTodosHandler addTodoHandler]]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [ring.middleware.json :refer [wrap-json-body]]))
 
 (defroutes app-routes
-  (GET "/" [] getAllTodos))
+  (GET "/all" [] getAllTodosHandler)
+  (POST "/new" [] addTodoHandler))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-defaults (wrap-json-body app-routes {:keywords? true}) api-defaults))
--- src/simple_http/todos.clj
+++ src/simple_http/todos.clj
@@ -1,12 +1,32 @@
 (ns simple-http.todos
   (:require [clojure.data.json :as json]))
 
-(def contentTypeHeader {"Content-Type" "text/json"})
-
 ; mutable collection
 (def todos (atom []))
+(defn- addTodo [title body]
+  (swap! todos conj {:title title
+                     :body body}))
+(defn- validateTodo [title body]
+  (or (some? title) (some? body)))
 
-(defn getAllTodos [req]
+; handlers
+(def contentTypeHeader {"Content-Type" "text/json"})
+
+(defn- errorResponse [status msg]
+  {:status status
+   :headers contentTypeHeader
+   :body (json/write-str {:error msg})})
+
+(defn getAllTodosHandler [req]
   {:status 200
    :headers contentTypeHeader
    :body (json/write-str @todos)})
+
+(defn addTodoHandler [req]
+  (let [body (req :body)]
+    (if (not (validateTodo (body :title) (body :body)))
+      (errorResponse 500 "wrong title or body length")
+      (addTodo (:title body) (:body body)))
+    {:status 200
+     :headers contentTypeHeader
+     :body (json/write-str {:id (count @todos)})}))
